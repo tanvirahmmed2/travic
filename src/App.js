6ea983ad8184b195b7342fcc2ed1dc5261b3bc59
tanvirahmmed2 <tanvir004006@gmail.com>
2025-07-06 12:45:31 +0600

    updtae

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
       <h1 className="text-4xl font-bold text-blue-600">Tailwind is working!</h1>
+      <h1 className="text-2xl text-red-700">This is a demo Pgae for react js</h1>
+      <p className="text-2xl italic text-green-800">Here installed: React js, Tailwind css, react icon, reatc dom, framer motion, formik</p>
     </div>
   );
 }
