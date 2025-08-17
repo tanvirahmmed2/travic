e4945f7b7b5c3127aeb073b92689025e1321ce57
Tanvir Ahmmed <139853229+tanvirahmmed2@users.noreply.github.com>
2025-08-17 16:59:17 +0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}", // must include js, jsx
   ],
-  theme: {},
+  theme: {
+    extend: {},
+  },
   plugins: [],
 }
