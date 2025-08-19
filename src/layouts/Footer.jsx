fb0bf9636b420aab8001a98543efaa0bada5e70b
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-08-19 14:59:54 +0600

    update

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
+    <section className='w-full px-2 '>
+        <footer className='w-full h-auto bg-gray-200 border-t-4 p-4 border-black rounded-lg flex flex-col items-center justify-center gap-4'>
+
+
+            <div>
+                <h1 className='text-2xl font-bold'>Daily News</h1>
+                <p>All right are reserved</p>
+            </div>
+
+        </footer>
+    </section>
   )
 }
 
