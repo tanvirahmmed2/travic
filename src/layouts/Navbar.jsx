b426d6c1bdd8750276360334a8cee20890ad9c2a
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-08-19 14:49:52 +0600

    update

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { FaBars, FaSearch } from "react-icons/fa";
 
 const Navbar = () => {
     return (
         <section className='w-full h-auto px-2 p-1'>
-            <nav className='w-full bg-indigo-500 text-white rounded-lg p-1 px-2 h-12 flex flex-row items-center justify-around'>
+            <nav className='w-full bg-cyan-700 text-white rounded-lg p-1 px-2 h-12 flex flex-row items-center justify-around'>
                 <a href="/" className='text-2xl font-bold'>Daily News</a>
 
                 <div className=' h-12 w-auto md:flex hidden flex-row items-center justify-center gap-1'>
@@ -21,6 +22,11 @@ const Navbar = () => {
                     <Link className='px-2 h-12 flex items-center justify-center hover:border-b-2 '>খেলাধুলা</Link>
                     <Link className='px-2 h-12 flex items-center justify-center hover:border-b-2 '>বিনোদন</Link>
                     <Link className='px-2 h-12 flex items-center justify-center hover:border-b-2 '>জব</Link>
+                </div>
+
+                <div className=' h-12 w-auto flex flex-row items-center justify-center gap-1'>
+                    <p className='px-6 h-12 flex items-center justify-center hover:border-b-2 '><FaSearch/></p>
+                    <p className='px-6 h-12 flex items-center justify-center hover:border-b-2 '><FaBars/></p>
                 </div>
 
             </nav>
