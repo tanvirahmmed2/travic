0e0844ec7476c754c3da467cb24620d4179fd188
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-10-23 16:36:17 +0600

    update

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
+    <section className='w-full flex flex-col itemc justify-between gap-4'>
+      <footer className='w-full flex flex-col md:flex-col items-center justify-center gap-4 bg-white'>
+
+      </footer>
+      <div className='w-full flex flex-col items-center justify-center  bg-cyan-700 text-white py-6 text-xs italic gap-2'>
+        <p>All rights are reserved by <a href="/" className='font-semibold'>Travic</a></p>
+        <p >A product of <a href="https://disibin.com" className='font-semibold'>Disibin</a></p>
+      </div>
+
+    </section>
   )
 }
 
