3a8064e0cb2c4f627f898193a6134a56f3a361e9
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-10-23 13:28:01 +0600

    update

--- src/components/Context.jsx
+++ src/components/Context.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { createContext } from 'react'
 
-const Context = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+
+export const ThemeContext= createContext()
+
+
+
+const ContextProvider = ({children}) => {
+
+
+
+
+  const ContextValue={
+
+  }
+
+  
+  return <ThemeContext.Provider value={ContextValue}>
+    {children}
+  </ThemeContext.Provider>
 }
 
-export default Context
+export default ContextProvider
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import "./index.css";
 import App from "./App";
+import ContextProvider from "./components/Context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+root.render(<ContextProvider>
+
   <BrowserRouter>
     <App />
   </BrowserRouter>
+</ContextProvider>
 );
