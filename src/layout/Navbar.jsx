b3feac314a1ceebdf87b8e02200f6b8f58aa321c
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-10-23 16:40:56 +0600

    update

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -9,18 +9,23 @@ const Navbar = () => {
 
 
   return (
-    <nav className='w-full h-16 backdrop-blur-3xl bg-white/70 fixed flex px-4 md:px-8 flex-row items-center justify-between'>
+    <nav className='w-full h-16 backdrop-blur-3xl bg-white/70 fixed flex px-4 md:px-8 flex-row items-center justify-around md:justify-between'>
       <a href="/" className=' font-semibold text-xl md:text-3xl sm:text-2xl'>
         Travic
       </a>
 
-      
+
       <div className='h-full w-auto hidden md:flex flex-row items-center justify-center gap-2'>
+        <Link className='h-full flex items-center justify-center px-4 hover:border-b-2 border-cyan-700' to='/'>Home</Link>
+        <Link className='h-full flex items-center justify-center px-4 hover:border-b-2 border-cyan-700' to='/'>Tour</Link>
+        <Link className='h-full flex items-center justify-center px-4 hover:border-b-2 border-cyan-700' to='/'>Campaign</Link>
+        <Link className='h-full flex items-center justify-center px-4 hover:border-b-2 border-cyan-700' to='/'>About</Link>
+        <Link className='h-full flex items-center justify-center px-4 hover:border-b-2 border-cyan-700' to='/'>Book</Link>
 
       </div>
 
       <div className='w-auto h-full flex flex-row items-center justify-center gap-2'>
-        <Link to='/login' className='h-8 rounded-lg flex items-center justify-center px-4 transition duration-500 ease-in-out hover:text-white hover:bg-sky-300 '>Login</Link>
+        <Link to='/login' className='h-8 rounded-lg flex items-center justify-center px-4 transition duration-500 ease-in-out hover:text-white hover:bg-cyan-700 '>Login</Link>
         <button onClick={()=>setMenu(!menu)} className='text-xl px-2'><MdOutlineMenu/></button>
       </div>
       
