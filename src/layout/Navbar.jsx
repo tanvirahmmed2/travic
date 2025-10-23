3a5ce137670fdc7e94f29928a94af34e7f55c00c
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-10-23 16:42:15 +0600

    update

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -17,10 +17,10 @@ const Navbar = () => {
 
       <div className='h-full w-auto hidden md:flex flex-row items-center justify-center gap-2'>
         <Link className='h-full flex items-center justify-center px-4 hover:border-b-2 border-cyan-700' to='/'>Home</Link>
-        <Link className='h-full flex items-center justify-center px-4 hover:border-b-2 border-cyan-700' to='/'>Tour</Link>
-        <Link className='h-full flex items-center justify-center px-4 hover:border-b-2 border-cyan-700' to='/'>Campaign</Link>
-        <Link className='h-full flex items-center justify-center px-4 hover:border-b-2 border-cyan-700' to='/'>About</Link>
-        <Link className='h-full flex items-center justify-center px-4 hover:border-b-2 border-cyan-700' to='/'>Book</Link>
+        <Link className='h-full flex items-center justify-center px-4 hover:border-b-2 border-cyan-700' to='/tour'>Tour</Link>
+        <Link className='h-full flex items-center justify-center px-4 hover:border-b-2 border-cyan-700' to='/campaign'>Campaign</Link>
+        <Link className='h-full flex items-center justify-center px-4 hover:border-b-2 border-cyan-700' to='/about'>About</Link>
+        <Link className='h-full flex items-center justify-center px-4 hover:border-b-2 border-cyan-700' to='/book'>Book</Link>
 
       </div>
 
