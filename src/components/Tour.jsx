81df6d8bafae06f9570b947e0c93b762c2a601cd
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-10-25 15:11:20 +0600

    update

--- src/components/Tour.jsx
+++ src/components/Tour.jsx
@@ -1,15 +1,20 @@
 import React, { useContext } from 'react'
 import { ThemeContext } from './Context'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useParams } from 'react-router-dom'
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
 
 const Tour = () => {
-  const {tours}= useContext(ThemeContext)
-  const {title}= useParams()
-  const data= tours.find((tour)=> tour.title=== title)
-  const {image, description, location,duration, price, rating, includes, highlights}=data
+  const { tours } = useContext(ThemeContext)
+  const { title } = useParams()
+  const data = tours.find((tour) => tour.title === title)
+  const { image, description, location, duration, price, rating, includes, highlights } = data
   return (
-    <div className='w-full h-full flex flex-col items-center justify-center gap-5 py-8 backdrop-blur-sm p-2 bg-white/60'>
-      <img src={image} alt=""  className='w-full md:w-3/4 max-h-[600px] rounded-lg'/>
+    <div className='w-full flex flex-col items-center justify-center gap-5 py-8 backdrop-blur-sm p-2 bg-white/60'>
+      <div className='w-full md:w-3/4 max-h-[600px] rounded-lg relative'>
+        <img src={image} alt="" />
+      </div>
       <h1 className='w-full text-center text-lg md:text-2xl lg:text-3xl font-semibold'>{title}</h1>
       <p>{description}</p>
     </div>
