ec6e7aa95a34e020535153c08d166bc929cb66ab
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-10-25 15:58:45 +0600

    update

--- src/components/Tour.jsx
+++ src/components/Tour.jsx
@@ -11,23 +11,21 @@ const Tour = () => {
   const data = tours.find((tour) => tour.title === title)
   const { image, description, location, duration, price, rating, includes, highlights } = data
   return (
-    <div className='w-full flex flex-col items-center justify-center gap-5 py-8 backdrop-blur-sm p-2 bg-white/60'>
+    <div className='w-full min-h-[800px] flex flex-col items-center justify-center gap-5 py-8 backdrop-blur-sm p-2 bg-white/60'>
       <div className='w-full md:w-3/4 rounded-lg overflow-hidden border-2 p-2 bg-white relative'>
         <img src={image} alt="" className='w-full max-h-[600px] object-cover rounded-lg ' />
         <p onClick={() => alert('Saved')} className='text-2xl md:text-4xl hover:scale-110 hover:text-pink-500 transition duration-500 ease-in-out cursor-pointer absolute top-4 right-4 text-white'><FontAwesomeIcon icon={faHeart} /></p>
       </div>
-      <div className='w-full flex flex-col items-center justify-center gap-4'>
-        <div className='w-full md:w-1/2 flex flex-col md:flex-row items-center justify-around'>
-          <p>{duration}</p>
-          <p> <FontAwesomeIcon icon={faStar} className='text-amber-500'/> {rating}</p>
-          <p><FontAwesomeIcon icon={faDollar}/> {price}</p>
-        </div>
-        <h1 className='w-full text-center text-lg md:text-2xl lg:text-3xl font-semibold'>{title} <span className='text-xs'>{location}</span></h1>
 
-      </div>
+
+      <h1 className='w-full text-center text-lg md:text-2xl lg:text-3xl font-semibold'>{title} <span className='text-xs'>{location}</span></h1>
+
+
       <p>{description}</p>
-      <div className='w-full grid grid-cols-1 md:grid-cols-2 justify-items-center'>
-        <div className='w-auto flex flex-col gap-2'>
+
+
+      <div className='w-full grid grid-cols-2 justify-items-center gap-4'>
+        <div className='w-full flex flex-col gap-2 bg-white p-4 rounded-md'>
           <h1 className='text-lg md:text-2xl font-semibold'>Special highlights</h1>
           {
             highlights.map((e) => {
@@ -35,7 +33,7 @@ const Tour = () => {
             })
           }
         </div>
-        <div>
+        <div className='w-full flex flex-col gap-2 bg-white p-4 rounded-md'>
           <h1 className='text-lg md:text-2xl font-semibold'>Package includes</h1>
           {
             includes.map((e) => {
@@ -43,6 +41,16 @@ const Tour = () => {
             })
           }
         </div>
+      </div>
+
+
+      <div className='w-full flex flex-col items-center justify-center gap-1'>
+        <div className='w-full text-center lg:w-1/2 flex flex-col md:flex-row items-center justify-center gap-1'>
+          <p className='px-6 p-1 bg-white w-full hover:text-pink-500 transition ease-in-out duration-500 cursor-pointer'>Duration: {duration}</p>
+          <p className='px-6 p-1 bg-white w-full hover:text-pink-500 transition ease-in-out duration-500 cursor-pointer'>Rating: <FontAwesomeIcon icon={faStar} className='text-amber-500' /> {rating}</p>
+          <p className='px-6 p-1 bg-white w-full hover:text-pink-500 transition ease-in-out duration-500 cursor-pointer'>Price: <FontAwesomeIcon icon={faDollar} /> {price}</p>
+        </div>
+        <button onClick={()=> alert('Purchased')} className='w-full lg:w-1/2 text-center bg-white p-1 hover:scale-110 transition duration-500 ease-in-out'>Purchase</button>
       </div>
     </div>
   )
