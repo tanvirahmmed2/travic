41be548a3050014ca2afc8aaa1ea90370ccc9e9d
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-10-25 14:46:39 +0600

    update

--- src/pages/Featured.jsx
+++ src/pages/Featured.jsx
@@ -6,19 +6,21 @@ const Featured = () => {
   const { tours } = useContext(ThemeContext)
   return (
     <section className='w-full flex flex-col items-center justify-center py-8'>
-      <div className='w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 justify-center p-2'>
+      <div className='w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 justify-center p-2'>
         {
           tours.slice(0, 6).map((tour) => {
             const { id, title, location, duration, image, category, description } = tour
-            return <div key={id} className='w-full border-2 bg-white relative overflow-hidden rounded-lg p-1'>
-              <img src={image} alt=""  className='w-full rounded-md'/>
+            return <div key={id} className='w-full border-2 bg-white relative overflow-hidden rounded-lg p-1 flex flex-col items-center justify-between gap-2'>
+              <img src={image} alt="" className='w-full rounded-md' />
               <p className='bg-white text-emerald-400 text-xs p-1 px-4 rounded-lg absolute top-3 left-3'>{category}</p>
-              <div>
-                <Link to={`/tours/${title}`}>{title}</Link>
-                <p>{location}</p>
-                <p>{description}</p>
+              <div className='w-full flex flex-col items-center justify-between p-1'>
+                <Link to={`/tours/${title}`} className='font-semibold text-base md:text-lg'>{title}</Link>
+                <p className='opacity-50 text-justify'>{description}</p>
               </div>
-              <p>{duration}</p>
+              <div className='w-full flex flex-row items-center justify-between p-1 text-xs'>
+                <p>{duration}</p>
+                <p>{location}</p>
+              </div>
             </div>
           })
         }
