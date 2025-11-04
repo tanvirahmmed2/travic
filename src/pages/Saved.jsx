89d62b643a20a54835a013efb466e3b9576e23ec
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-11-04 18:33:10 +0600

    update

--- src/pages/Saved.jsx
+++ src/pages/Saved.jsx
@@ -1,12 +1,30 @@
 import React, { useContext } from 'react'
+import { Link } from 'react-router-dom'
 import { ThemeContext } from '../components/Context'
+import { MdDeleteOutline } from 'react-icons/md'
 
 const Saved = () => {
-  const {user}= useContext(ThemeContext)
-  const savedData= user.saved
+  const { user } = useContext(ThemeContext)
+  const savedData = user.saved
+  if (savedData === null) return <p>no tour saved</p>
   return (
-    <div>
-      
+    <div className='w-full max-w-[600px] flex flex-col items-center justify-center gap-6 mt-6 '>
+      <p className='text-3xl font-semibold text-center text-sky-400'>Enjoy your safe tour with Travic</p>
+      <div className='w-full flex flex-col items-center justify-center rounded-lg shadow-sm gap-4 bg-white p-2'>
+        <h1 className='text-2xl font-semibold border-b-2 py-2'>Saved tours</h1>
+        
+        {
+          savedData.map((tour) => {
+            const { title, tourId } = tour
+            return <div key={tourId} className='w-full flex flex-row items-center justify-between  '>
+              <Link to={`/tours/${title}`} >{title}</Link>
+              <p><MdDeleteOutline /></p>
+            </div>
+          })
+        }
+      </div>
+
+
     </div>
   )
 }
